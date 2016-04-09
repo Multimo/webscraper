4c34baf2bc924d30fe995769aca23c24bb78703a
Tim Mulqueen <tim.mulqueen@gmail.com>
2016-04-09 19:12:36 +0000

    filled out the links api route, added transformer middle ware to response object. needs to filter out whitespace characters and then should be good.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 //TODO: to search through a links in navigationa and construct a list of pages to iterate through
-
+//TODO: hide unwated text on click? in frontend
 //TODO: output format to either output.json or send as a responce to app.js
 //TODO: Validation on the url sent "http//:"
 //TODO: Make requests async / non blocking so more than one request can happen at same time. Promises?
@@ -13,7 +13,7 @@ var cheerio = require('cheerio');
 var app = express();
 
 var Xray = require('x-ray');  
-var x = Xray();
+var xray = new Xray();
 
 var bodyParser = require('body-parser');
 var router = express.Router();
@@ -26,16 +26,23 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
+// middle ware to solve the access-controll-allow-origin issue with the api
+app.use(function(req, res, next) { 
+    res.header('Access-Control-Allow-Origin', "*"); 
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+    next();
+});
+    
 // makes /client the root of the routes       
 app.use(express.static('client'));
 
 // all calls to server will pass through this function and the middle ware above
 router.use(function(req, res, next) {
     console.log("Something is happening");
-
     next();
 });
-
+//basic get request. used for testing
 router.get('/', function(req, res) {
     console.log('working?')
     res.json({
@@ -43,27 +50,20 @@ router.get('/', function(req, res) {
     });
    
 });
-
+//default post route, returns a single pages content
 router.post("/", function(req, res, next) {
-    
+    //catpure and log the url in the request
     var url = req.body.url;
-    console.log(url);
+    console.log(req.body);
     var outPut;
-    
-    links = x(url, 'a', [{
-  title: '',
-  href: '@href',
-  }])(function(err, a) {
-               res.send(links); 
-           });
-    
+ 
 
 
-    
+    //begin request to url
     request({
         url: url,
         headers: {
-            'User-Agent': 'tim.mulqueen@gmail.com',
+             'User-Agent': 'tim.mulqueen@gmail.com',
         }
 
     }, function(error, response, html) {
@@ -73,12 +73,7 @@ router.post("/", function(req, res, next) {
             var $ = cheerio.load(html, {
                 ignoreWhitespace: true
             });
-            // where to store text from site
-            // var json = [];
-            // var testoutPut = $("script").contents().filter(function(){
-            //     return (this.name === 'script');
-            // })
-                
+            
             // this takes all tags from within the body tag except scripts returns all the text
             // and then cleans it up into a nice array ["Home", "About", "Services", etc.]
             outPut = $('body *')
@@ -95,48 +90,76 @@ router.post("/", function(req, res, next) {
               }
               return all;
              }, []);
-          
+             
+             res.send(outPut);
             
-            // links = $('a')
-            // .reduce(fucntion(all, item, index) {
-                
-            // });
-            // console.log(links);
-            
-        //     x($, 'title')(function(err, title) {
-        //       console.log(title); 
-        //   });
+         ;
            
            
             //  console.log( JSON.stringify(outPut, null, 2));
           
+            // var string = outPut.map(function() {
+            //   return this.replace( 'Montreal', '%WORKCITY%' );
+            //  });
 
+            
+            
 
-
-             jsonfile.writeFile('output.json', outPut, function(error) {
-            if (error) {
-                console.log("write error:  " + error.message);
-            } else {
-                console.log('File successfully written! - Check your project directory for the output.json file');
-            }
-            res.send(outPut);
-           
-        })
-
-        } else res.send("hello! something went wrong!" + error);
+        } else res.send("hello something went wrong! Error: %s", error);
 
         // writes file to output.json needs content to be stringified
 
 
     })
 });
+// links api route returns links
+router.post("/links", function(req, res, next){
+    //puts url in a var and logs it
+    var linkUrl = req.body.url;
+    var id = req.body.$id
 
+    var linkOutPut;
+    
+    //transform string of url into a regex
+    var urlRegex = new RegExp(linkUrl);
+    var invalidEntries = 0;
+    //need two transformer on the results
+    // 1: to filter links based on url sent in filter out /# and any without url
+    // 2: filter out any \t \n from results .replace .trim
+    // 3: filter out any text nodes in a link
+    function filterLinks(obj) {
+      if ( obj.a && obj.href.search(urlRegex) != -1 ) {
+        obj.a.trim();
+        return true;
+      } else {
+        invalidEntries++;
+        return false;
+      }
+    }
+    
+    
+    //needs to go to each link in a page and if the link shares characters with the url var
+    // it should return an object of the links
+    console.log('hitting the xray with %s', linkUrl);
+    linkOutPut = xray( linkUrl, 'a', [
+     {
+      a: '',
+      href: '@href' 
+    }])(function(err, results) {
+        if (!err) {
+            var data = results.filter(filterLinks);
+            console.log('there were %s entrieds filtered', invalidEntries);
+           
+            res.send(data);
+        } else console.log('erroer: %s', err);
+    })
+    
+    
 
-
-
+});
 // sets /client to router as  above
 app.use('/client', router);
 
-app.listen('8081')
+app.listen('8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
